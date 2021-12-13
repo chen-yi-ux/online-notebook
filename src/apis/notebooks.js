042d4ecc4676ce1a8b2c0160c303d70943ef6bc4
Cyer <1678447359@qq.com>
2021-12-13 22:23:08 +0800

    笔记本列表页面：封装 Notebook 的API，完成数据交互

---
+++ src/apis/notebooks.js
@@ -0,0 +1,37 @@
+import request from '@/helpers/request'
+import {friendlyDate} from '@/helpers/util'
+
+const URL = {
+  GET: '/notebooks',
+  ADD: '/notebooks',
+  UPDATE: '/notebooks/:id',
+  DELETE: '/notebooks/:id'
+}
+
+export default {
+  getAll() {
+    return new Promise((resolve, reject) => {
+      request(URL.GET)
+        .then(res => {
+          res.data = res.data.sort((notebook1, notebook2) => {
+            return notebook2.id - notebook1.id
+          })
+          res.data.forEach(notebook => {
+            notebook.createdAtFriendly = friendlyDate(notebook.createdAt)
+          })
+          resolve(res)
+        }).catch(err => {
+        reject(err)
+      })
+    })
+  },
+  updateNotebook(notebookId, {title = ''} = {title: ''}) {
+    return request(URL.UPDATE.replace(':id', notebookId), 'PATCH', {title})
+  },
+  deleteNotebook(notebookId) {
+    return request(URL.DELETE.replace(':id', notebookId), 'DELETE')
+  },
+  addNotebook({title = ''} = {title: ''}) {
+    return request(URL.ADD, 'POST', {title})
+  }
+}
--- src/components/Notebooks.vue
+++ src/components/Notebooks.vue
@@ -2,7 +2,7 @@
   <div class="page">
     <SideBar/>
     <div class="content">
-      <header>
+      <header @click="onCreate">
         <svg>
           <use xlink:href="#icon-add"></use>
         </svg>
@@ -11,35 +11,21 @@
       <div class="xxx"></div>
       <main>
         <div class="layout">
-          <h3>笔记本列表（0）</h3>
+          <h3>笔记本列表（{{ notebooks.length }}）</h3>
           <div class="list">
-            <router-link to="/note/1" class="notebook">
+            <router-link to="/note/1" class="notebook" v-for="notebook in notebooks" :key="notebook.id">
               <div class="item">
                 <div class="left">
                   <svg>
                     <use xlink:href="#icon-notebook1"></use>
                   </svg>
-                  <span class="name">第一个笔记本</span>
-                  <span class="number">(1)</span>
+                  <span class="name">{{ notebook.title }}</span>
+                  <span class="number">({{ notebook.noteCounts }})</span>
                 </div>
                 <div class="right">
-                  <span class="wrapper">1天前</span>
-                  <span class="wrapper" @click.stop.prevent="onEdit">编辑</span>
-                  <span class="wrapper" @click.stop.prevent="onDelete">删除</span>
-                </div>
-              </div>
-              <div class="item">
-                <div class="left">
-                  <svg>
-                    <use xlink:href="#icon-notebook1"></use>
-                  </svg>
-                  <span class="name">第一个笔记本</span>
-                  <span class="number">(1)</span>
-                </div>
-                <div class="right">
-                  <span class="wrapper">1天前</span>
-                  <span class="wrapper" @click.stop.prevent="onEdit">编辑</span>
-                  <span class="wrapper" @click.stop.prevent="onDelete">删除</span>
+                  <span class="wrapper">{{notebook.createdAtFriendly}}</span>
+                  <span class="wrapper" @click.stop.prevent="onEdit(notebook)">编辑</span>
+                  <span class="wrapper" @click.stop.prevent="onDelete(notebook)">删除</span>
                 </div>
               </div>
             </router-link>
@@ -53,11 +39,15 @@
 <script>
 import SideBar from "./SideBar"
 import Auth from '@/apis/auth'
+import Notebooks from '@/apis/notebooks'
+import {friendlyDate} from "@/helpers/util"
 
 export default {
   components: {SideBar},
   data() {
-    return {}
+    return {
+      notebooks: []
+    }
   },
   created() {
     Auth.getInfo()
@@ -66,13 +56,53 @@ export default {
           this.$router.push({path: '/login'})
         }
       })
+
+    Notebooks.getAll()
+      .then(res => {
+        this.notebooks = res.data
+      })
   },
   methods: {
-    onEdit() {
-      console.log('编辑')
+    onCreate() {
+      this.$prompt('请输入笔记本标题', '创建笔记本', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: '标题不能为空，且不超过30个字符串'
+      }).then(({value}) => {
+        Notebooks.addNotebook({title: value})
+        .then(res => {
+          res.data.createdAtFriendly = friendlyDate(res.data.createdAt)
+          res.data.noteCounts = 0
+          this.notebooks.unshift(res.data)
+        })
+      })
     },
-    onDelete() {
-      console.log('删除')
+    onEdit(notebook) {
+      this.$prompt('请输入笔记本标题', '修改笔记本', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /^.{1,30}$/,
+        inputValue: notebook.title,
+        inputErrorMessage: '标题不能为空，且不超过30个字符串'
+      }).then(({value}) => {
+        Notebooks.updateNotebook(notebook.id, {title: value})
+        notebook.title = value
+      })
+    },
+    onDelete(notebook) {
+      this.$confirm('确定要删除该笔记本吗?', '删除笔记本', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        Notebooks.deleteNotebook(notebook.id)
+        this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      })
     }
   }
 }
