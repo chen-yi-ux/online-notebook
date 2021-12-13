2d0c99b1f583f692355e0c114da33908ed606beb
Cyer <1678447359@qq.com>
2021-12-13 18:00:15 +0800

    生产环境和开发环境baseURL 无缝切换

---
+++ build/mock.config.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+const mockBaseURL = 'https://localhost:8080'
+const realBaseURL = 'https://note-server.hunger-valley.com'
+
+exports.config = function({isDev = true} = {isDev: true}){
+  let fileText = `
+  module.exports = {
+    baseURL: '${isDev ? mockBaseURL : realBaseURL}'
+  }
+  `
+  fs.writeFileSync(path.join(__dirname, '../src/helpers/config-baseURL.js'), fileText)
+}
