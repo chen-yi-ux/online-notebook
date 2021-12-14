4f4c3b830dd7cde1225eab631fe3a67c3ea14e77
Cyer <1678447359@qq.com>
2021-12-14 20:13:38 +0800

    NoteSidebar 组件的数据交互和功能

---
+++ src/apis/notes.js
@@ -0,0 +1,38 @@
+import request from '@/helpers/request'
+import {friendlyDate} from "@/helpers/util"
+
+const URL = {
+  GET: '/notes/from/:notebookId',
+  ADD: '/notes/to/:notebookId',
+  UPDATE: '/notes/:noteId',
+  DELETE: '/notes/:noteId'
+}
+
+export default {
+  getAll({notebookId}) {
+    return new Promise((resolve, reject) => {
+      request(URL.GET.replace(':notebookId', notebookId))
+        .then(res => {
+          res.data.forEach(note => {
+            note.updatedAtFriendly = friendlyDate(note.updatedAt)
+            note.createdAtFriendly = friendlyDate(note.createdAt)
+          })
+          res.data = res.data.sort((note1, note2) => {
+            return note2.updatedAt - note1.updatedAt
+          })
+          resolve(res)
+        }).catch(err => {
+        reject(err)
+      })
+    })
+  },
+  updateNote({noteId}, {title, content}) {
+    return request(URL.UPDATE.replace(':noteId', noteId), 'PATCH', {title, content})
+  },
+  deleteNote({noteId}) {
+    return request(URL.DELETE.replace(':noteId', noteId), 'DELETE')
+  },
+  addNote({notebookId}, {title = '', content = ''} = {title: '', content: ''}) {
+    return request(URL.ADD.replace(':notebookId', notebookId), 'POST', {title, content})
+  }
+}
--- src/components/NoteSidebar.vue
+++ src/components/NoteSidebar.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="note-sidebar">
     <span class="btn add-note">添加笔记</span>
-    <el-dropdown class="notebook-title" placement="bottom">
+    <el-dropdown class="notebook-title" placement="bottom" @command="handleCommand">
       <span class="el-dropdown-link">
-        sdwewe
+        {{ curBook.title }}
         <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>
-          dsad
+        <el-dropdown-item v-for="notebook in notebooks" :command="notebook.id" :key="notebook.id">
+          {{ notebook.title }}
         </el-dropdown-item>
         <el-dropdown-item command="trash">回收站</el-dropdown-item>
       </el-dropdown-menu>
@@ -18,16 +18,10 @@
       <div>标题</div>
     </div>
     <div class="notes">
-      <div>
-        <router-link to="/note/1">
-          <span>11</span>
-          <span>22</span>
-        </router-link>
-      </div>
-      <div>
-        <router-link to="/note/1">
-          <span>11</span>
-          <span>22</span>
+      <div v-for="note in notes" :key="note.noteId">
+        <router-link :to="`/note?noteId=${note.id}&notebookId=${curBook.id}`">
+          <span>{{ note.updatedAtFriendly }}</span>
+          <span>{{ note.title }}</span>
         </router-link>
       </div>
     </div>
@@ -35,7 +29,39 @@
 </template>
 
 <script>
-export default {}
+import Notebooks from '@/apis/notebooks'
+import Notes from '@/apis/notes'
+
+export default {
+  data() {
+    return {
+      notebooks: [],
+      notes: [],
+      curBook: {}
+    }
+  },
+  created() {
+    Notebooks.getAll()
+      .then(res => {
+        this.notebooks = res.data
+        this.curBook = this.notebooks.find(notebook => notebook.id === parseInt(this.$route.query.notebookId)) || this.notebooks[0] || {}
+        return Notes.getAll({notebookId: this.curBook.id})
+      }).then(res => {
+      this.notes = res.data
+    })
+  },
+  methods: {
+    handleCommand(notebookId) {
+      if (notebookId === 'trash') {
+        return this.$router.push({path: '/trash'})
+      }
+      Notes.getAll({notebookId})
+        .then(res => {
+          this.notes = res.data
+        })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
