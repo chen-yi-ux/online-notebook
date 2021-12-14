53cb22bb658ae1fb0bbfa09cc39a660f28fed631
Cyer <1678447359@qq.com>
2021-12-14 19:08:39 +0800

    完成 TrashDetail 组件的样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -3,32 +3,18 @@
     <NoteSidebar/>
     <div class="note-detail">
       <div class="note-bar">
-        <div>
-          <span>创建日期：1天前</span>
-          <span>更新日期：刚刚</span>
-          <span>未保存</span>
-        </div>
-        <div class="action">
-          <div class="icon">
-            <svg>
-              <use xlink:href="#icon-yulan"></use>
-            </svg>
-            预览
-          </div>
-          <div class="icon">
-            <svg>
-              <use xlink:href="#icon-shanchu"></use>
-            </svg>
-            删除
-          </div>
-        </div>
+        <span>创建日期：1天前</span>
+        <span>更新日期：刚刚</span>
+        <span>未保存</span>
+        <span class="action">删除</span>
+        <span class="action">预览</span>
       </div>
       <div class="note-title">
         <input type="text" placeholder="笔记本标题">
       </div>
       <div class="note-content">
         <textarea placeholder="输入内容，支持 markdown 语法"></textarea>
-<!--        <div class="preview markdown-body" v-html="previewContent"></div>-->
+        <!--        <div class="preview markdown-body" v-html="previewContent"></div>-->
       </div>
     </div>
   </div>
@@ -58,9 +44,4 @@ export default {
 
 <style lang="less" scoped>
 @import url(../assets/css/NoteDetail.less);
-
-svg {
-  width: 20px;
-  height: 20px;
-}
 </style>
--- src/components/TrashDetail.vue
+++ src/components/TrashDetail.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="trash">
-    noteId: {{ msg }}
+    <div class="note-sidebar">
+      <span class="notebook-title">
+        回收站
+      </span>
+      <div class="menu">
+        <div>更新时间</div>
+        <div>标题</div>
+      </div>
+      <div class="notes">
+        <div>
+          <router-link to="/trash/1">
+            <span>11</span>
+            <span>22</span>
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span>创建日期：1天前</span>
+        <span>更新日期：5分钟前</span>
+        <span>所属笔记本：第一个</span>
+        <span class="action">彻底删除</span>
+        <span class="action">恢复</span>
+      </div>
+      <div class="note-title">
+        <span>第一个笔记</span>
+      </div>
+      <div class="note-content">
+        <div class="preview markdown-body" v-html="compileMarkdown"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Auth from '@/apis/auth'
+import MarkdownIt from 'markdown-it'
+
+let md = new MarkdownIt()
 
 export default {
   data() {
@@ -24,8 +58,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+@import url(../assets/css/NoteSidebar.less);
+@import url(../assets/css/NoteDetail.less);
+
 .trash {
-  color: red;
+  display: flex;
+  align-items: stretch;
+  flex: 1;
 }
 </style>
