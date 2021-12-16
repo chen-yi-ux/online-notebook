6400d08927846d3d85985a1e7413623bf6867985
Cyer <1678447359@qq.com>
2021-12-16 23:24:48 +0800

    修改 BaseURL

--- build/mock.config.js
+++ build/mock.config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 // const mockBaseURL = 'https://localhost:8080'
-const mockBaseURL = '//note-server.hunger-valley.com'
-const realBaseURL = '//note-server.hunger-valley.com'
+const mockBaseURL = 'https://note-server.hunger-valley.com'
+const realBaseURL = 'https://note-server.hunger-valley.com'
 
 exports.config = function({isDev = true} = {isDev: true}){
   let fileText = `
